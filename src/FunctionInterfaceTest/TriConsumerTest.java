653aa53038675479f0030e09fef1651ed1fa9a4f
mz01-junghunee <cjswovkdnj12@naver.com>
2024-01-25 09:37:47 +0900

    커스텀 펑셔널 인터페이스 테스트

--- src/FunctionInterfaceTest/TriConsumerTest.java
+++ src/FunctionInterfaceTest/TriConsumerTest.java
@@ -2,8 +2,24 @@ package FunctionInterfaceTest;
 
 public class TriConsumerTest {
     public static void main(String[] args) {
+
+        System.out.println("인터페이스를 익명 객체로써 구현 :::: ");
+        TriConsumer<Integer,String,Long> function = new TriConsumer<>(){
+            @Override
+            public void accept(Integer a, String b, Long c) {
+                System.out.println("int : " + a + " , String : " +b + " , " + "Long : " + c);
+            }
+        };
+        good(function);
+        System.out.println();
+
+        System.out.println("인터페이스를 람다식으써 구현 :::: ");
         good((a,b,c)-> System.out.println("int : " + a + " , String : " +b + " , " + "Long : " + c));
+        System.out.println();
+
+        System.out.println("인터페이스를 메소드 레퍼런스로써 구현 :::: ");
         good(TriConsumerTest::function);
+        System.out.println();
     }
 
     private static void good(TriConsumer<Integer,String,Long> function){
