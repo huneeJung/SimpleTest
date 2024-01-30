02158b814a22402f8f0ea9f42fe96a24970ac422
mz01-junghunee <cjswovkdnj12@naver.com>
2024-01-30 18:20:34 +0900

    자바 스크립트 코드 테스트, Java 15 이후 부터 JavaScript 엔진을 지원하지 않으며, 파이썬과 자바스크립트 등의 다른 언어를 실행하려면 별도의 엔진 라이브러리를 사용해야함

--- src/ScriptEnginManagerTest.java
+++ src/ScriptEnginManagerTest.java
@@ -5,7 +5,7 @@ import javax.script.ScriptException;
 public class ScriptEnginManagerTest {
     public static void main(String[] args) throws ScriptException {
         ScriptEngineManager engineManager = new ScriptEngineManager();
-        ScriptEngine engine = engineManager.getEngineByName("JavaScript");
+        ScriptEngine engine = engineManager.getEngineByName("JavaScript"); // 자바 15 이후 자바스크립트는 사용불가
         //engine.eval("console.log('Test')"); console.log 는 브라우저에서 동작하므로 에러 발생
         // print 는 ScriptEngine 이 제공하는 것으로, 자바의 System.print 를 사용하여 스크립트에서 실행한 데이터를 자바의 콘솔에 출력
         engine.eval("" +
