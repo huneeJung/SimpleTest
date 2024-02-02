c58d8ccd49fbaa6aa8ffa68315fedf8b32b716cc
mz01-junghunee <cjswovkdnj12@naver.com>
2024-02-02 14:21:33 +0900

    SerializationTest - Jackson 테스트 Jackson 은 Java 8 이전 버전의 호환성을 유지하기 위해, java.time 하위 클래스의 인스턴스를 직렬화하지 못하게 되어 있으므로 별도의 설정과 더불어 포맷팅하여 직렬화 수행, 또는 JavaTimeModule 을 mapper 에 추가해서 java.time 하위 클래스타입도 직렬화 가능하도록 설정할 수 있음

--- src/SerializationTest/JacksonTest.java
+++ src/SerializationTest/JacksonTest.java
@@ -1,9 +1,11 @@
 package SerializationTest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.cfg.SerializerFactoryConfig;
 import com.fasterxml.jackson.databind.ser.BeanSerializerFactory;
 import com.fasterxml.jackson.databind.ser.SerializerFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 import java.time.LocalDateTime;
@@ -13,14 +15,17 @@ import java.util.List;
 
 public class JacksonTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializerFactory(new CustomBeanSerializerFactory(null));
+        mapper.registerModule(new JavaTimeModule());
+//        mapper.setSerializerFactory(new CustomBeanSerializerFactory(null));
         People people = new People(0,"Kim",LocalDateTime.now());
         people.getPeopleList().add(new People(0,"Kim2",LocalDateTime.now()));
         people.getPeopleList().add(new People(0,"Kim3",LocalDateTime.now()));
-        JsonNode json = mapper.valueToTree(people);
+        String json = mapper.writeValueAsString(people);
         System.out.println(json.toString());
+        People people2 = mapper.readValue(json,People.class);
+        System.out.println(people2);
     }
 
 
@@ -30,6 +35,10 @@ public class JacksonTest {
         protected CustomBeanSerializerFactory(SerializerFactoryConfig config) {
             super(config);
         }
+        // java 의 인스턴스를 json 형태의 text 로 변활할때 찾는 객체로써, jackson 은 각 타입마다 내부적으로 갖고 있는 JsonSerializer 가 존재하는데,
+        // java 8 이전 버전의 호환성을 유지하기 위해 java.time 하위 클래스들은 JsonSerializer 를 별도로 갖고 있지 않아 에러가 발생, 그래서 별도로 JsonSerializer 를 생성해서 넣어줌
+        // jackson-datatype-jsr310 ( JavaTimeModule ) 모듈을 사용하여도 java.time 하위 패키지에 존재하는 클래스 타입을 직렬화 할 수 있음
+        @Override
         protected JsonSerializer<?> _findUnsupportedTypeSerializer(SerializerProvider ctxt, JavaType type, BeanDescription beanDesc) throws JsonMappingException {
             if (type.getRawClass() == LocalDateTime.class) {
                 return localDateTimeSerializer;
@@ -49,7 +58,10 @@ public class JacksonTest {
         private LocalDateTime birthDay;
         private List<People> peopleList = new ArrayList<>();
 
-        People(int age,String name,LocalDateTime birthDay){
+        public People() {
+        }
+
+        People(int age, String name, LocalDateTime birthDay){
             this.age = age;
             this.name = name;
             this.birthDay = birthDay;
@@ -81,6 +93,16 @@ public class JacksonTest {
 
         public void setPeopleList(List<People> peopleList) {
             this.peopleList = peopleList;
+        }
+
+        @Override
+        public String toString() {
+            return "People{" +
+                    "age=" + age +
+                    ", name='" + name + '\'' +
+                    ", birthDay=" + birthDay +
+                    ", peopleList=" + peopleList.toString() +
+                    '}';
         }
     }
 }
