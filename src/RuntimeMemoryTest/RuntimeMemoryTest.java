ae7d8ec4c12e8590cf0a1d74ba39d347191853d9
mz01-junghunee <cjswovkdnj12@naver.com>
2024-02-21 16:10:12 +0900

    런타임 힙 메로리 테스트 : JVM 에서 할당받은 힙 메모리 총량과, 현재 설정으로써 사용가능한 힙메모리등 확인

---
+++ src/RuntimeMemoryTest/RuntimeMemoryTest.java
@@ -0,0 +1,15 @@
+package RuntimeMemoryTest;
+
+public class RuntimeMemoryTest {
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+
+        var freeMemory = runtime.freeMemory(); // 사용 가능한 힙 메모리 총량 / 바이트 단위
+        var totalMemory = runtime.totalMemory(); // 사용 할 수 있는 힙 메모리 총량 / 바이트 단위
+        var maxMemory = runtime.maxMemory(); // 운영체제로부터 JVM 에 할당받은 힙 메모리의 총량 / 바이트 단위
+        System.out.println((double)freeMemory/(double)(1024*1024));
+        System.out.println((double)maxMemory/(double)(1024*1024));
+        System.out.println((double)totalMemory/(double)(1024*1024));
+        System.out.println("Used Memory : "+(double)(totalMemory-freeMemory)/(double)(1024*1024));
+    }
+}
